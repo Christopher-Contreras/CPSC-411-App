50952925fe78f0a9ce1b2c4525e7494edc88050d
csuftitan <csuftitan@MacBook-Pro-266.local>
2025-03-19 18:39:03 -0700

    initialization

---
+++ ExpensesTracker/ExpensesTracker/ExpensesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesTrackerApp.swift
+//  ExpensesTracker
+//
+//  Created by csuftitan on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
