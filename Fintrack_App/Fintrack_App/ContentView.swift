8f9f8773cabd24ca48d0e861550d994c43c79ef1
csuftitan <csuftitan@MacBook-Pro-266.local>
2025-03-25 13:00:35 -0700

    initialization

---
+++ Fintrack_App/Fintrack_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fintrack_App
+//
+//  Created by csuftitan on 3/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fintrack_App/Fintrack_App/Fintrack_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fintrack_AppApp.swift
+//  Fintrack_App
+//
+//  Created by csuftitan on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Fintrack_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
